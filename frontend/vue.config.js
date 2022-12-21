78d8c83832bacdb5e12cec0eb3a01c3767d3125c
Rakhmatullin Ramazan <koscheibismark@yandex.ru>
2022-12-21 19:08:05 +0300

    fix public path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/~s335109/WEB-Lab4" : "/",
+  publicPath: './',
 });
